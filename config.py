007490136097470f7b94e6817f2df747da8b8026
EdoardoMosca <edoardo@liquid.ai>
2025-08-06 13:03:41 +0000

    more generic output dir

--- config.py
+++ config.py
@@ -72,7 +72,7 @@ Args:
 """
 
 JOB_CONFIG = JobConfig(
-    job_name="edo_tests",
+    job_name="my_output_dir",
     model_name="LFM2-1.2B",
     training_type="sft",
     dataset=smoltalk_dataset,

79a101bb04b9c215cbb2ed977aa9b8f98b037808
alay2shah <alay0shah@gmail.com>
2025-10-09 09:19:35 +0900

    wandb flag

--- config.py
+++ config.py
@@ -65,6 +65,7 @@ user_config = {
     "per_device_train_batch_size": None,
     "gradient_accumulation_steps": None,
     "learning_rate": None,
+    "wandb_logging": False, # If set to True, set your wandb API key via 'export WANDB_API_KEY=<your_api_key>'
 }
 
 

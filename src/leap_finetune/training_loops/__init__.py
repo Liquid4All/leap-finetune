c98848a0a8ba358fe146548eb4656b33fd6d7266
alay2shah <alay0shah@gmail.com>
2025-12-19 14:57:41 +0000

    training loop map

---
+++ src/leap_finetune/training_loops/__init__.py
@@ -0,0 +1,13 @@
+"""Training loops for different training types."""
+
+from .dpo_run import dpo_run
+from .sft_run import sft_run
+from .vlm_sft_run import vlm_sft_run
+
+TRAINING_LOOPS = {
+    "sft": sft_run,
+    "dpo": dpo_run,
+    "vlm_sft": vlm_sft_run,
+}
+
+__all__ = ["TRAINING_LOOPS", "sft_run", "dpo_run", "vlm_sft_run"]
